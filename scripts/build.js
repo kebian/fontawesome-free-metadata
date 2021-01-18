b7156007d3ed4d81887fc26935485aca1832b564
Rob Stiles <robs@codexsoftware.co.uk>
2021-01-18 13:47:14 +0000

    Initial commit

---
+++ scripts/build.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const YAML = require('yaml')
+const faModule = '@fortawesome/fontawesome-free'
+const metaPath = path.dirname(require.resolve(`${faModule}${path.sep}package.json`)) + `${path.sep}metadata`
+const groups = fs.readdirSync(metaPath).map(filename => path.basename(filename, path.extname(filename)))
+let indexJs = ''
+
+for (const group of groups) {
+    const filename = require.resolve(`${metaPath}${path.sep}${group}.yml`)
+    const file = fs.readFileSync(filename, 'utf8')
+    const metaData = JSON.stringify(YAML.parseDocument(file).toJSON())
+    const js = `const data = ${metaData}\nmodule.exports = data\n`
+    fs.writeFileSync(`data${path.sep}${group}.js`, js)
+    indexJs += `const ${group} = require('./${group}')\nmodule.exports.${group} = ${group}\n\n`
+}
+
+fs.writeFileSync(`data${path.sep}index.js`, indexJs)
